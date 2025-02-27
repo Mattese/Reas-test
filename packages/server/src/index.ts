80e6f178a3e59fbf3e33f61e86764513d58c3785
Martin Reichl <reichlm@seznam.cz>
2025-02-27 17:19:50 +0100

    chore: add cors to server

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -2,11 +2,12 @@ import express from "express";
 import leadRoutes from "./routes/lead";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db";
+import cors from "cors";
 
 dotenv.config();
 const app = express();
 // connectDB();
-
+app.use(cors());
 app.use(express.json());
 app.use("/api", leadRoutes);
 
